1dd07a184a5ba8c112b6126405cdcecbc9ceb264
Tiago <tiagolourinho001@gmail.com>
2023-01-02 12:32:23 +0000

    changed file

--- source/cloud/instance.py
+++ source/cloud/instance.py
@@ -4,7 +4,7 @@ import boto3
 from botocore.exceptions import ClientError
 
 logger = logging.getLogger(__name__)
-ec2 = boto3.resource('ec2')
+ec2 = boto3.resource("ec2")
 
 
 def create(image, instance_type, key_pair, security_groups=None):
@@ -31,17 +31,24 @@ def create(image, instance_type, key_pair, security_groups=None):
     """
     try:
         instance_params = {
-            'ImageId': image, 'InstanceType': instance_type, 'KeyName': key_pair
+            "ImageId": image,
+            "InstanceType": instance_type,
+            "KeyName": key_pair,
         }
         if security_groups is not None:
-            instance_params['SecurityGroupIds'] = [sg for sg in security_groups]
+            instance_params["SecurityGroupIds"] = [sg for sg in security_groups]
         instance = ec2.create_instances(**instance_params, MinCount=1, MaxCount=1)[0]
         instance.wait_until_running()
     except ClientError as err:
         logging.error(
             "Couldn't create instance with image %s, instance type %s, and key %s. "
-            "Here's why: %s: %s", image, instance_type, key_pair,
-            err.response['Error']['Code'], err.response['Error']['Message'])
+            "Here's why: %s: %s",
+            image,
+            instance_type,
+            key_pair,
+            err.response["Error"]["Code"],
+            err.response["Error"]["Message"],
+        )
         raise
     else:
         return instance
@@ -53,7 +60,7 @@ def display(instance):
     """
     try:
         instance.load()
-        ind = '\t'
+        ind = "\t"
         print(f"{ind}ID: {instance.id}")
         print(f"{ind}Image ID: {instance.image_id}")
         print(f"{ind}Instance type: {instance.instance_type}")
@@ -63,17 +70,25 @@ def display(instance):
     except ClientError as err:
         logger.error(
             "Couldn't display your instance. Here's why: %s: %s",
-            err.response['Error']['Code'], err.response['Error']['Message'])
+            err.response["Error"]["Code"],
+            err.response["Error"]["Message"],
+        )
         raise
 
 
-if __name__ == '__main__':
-    if sys.argv[1] == 'launch':
-        display(create("ami-0283a57753b18025b", "t2.micro", "esc22keypair", ["sg-002f71db95c7f849d"]))
-    elif sys.argv[1] == 'terminate':
+if __name__ == "__main__":
+    if sys.argv[1] == "launch":
+        display(
+            create(
+                "ami-07c2ae35d31367b3e",
+                "t2.micro",
+                "T2M_key_portatil",
+                ["sg-01bec21cb33b3f507"],
+            )
+        )
+    elif sys.argv[1] == "terminate":
         ec2.Instance(sys.argv[2]).terminate()
     else:
         print("Syntax (two modes):")
         print("\tinstance.py create")
         print("\tinstance.py terminate <instance id>")
-
